1af44ccd48fc250f0833235f99af8642b5f55788
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-02-28 16:17:29 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int year;
+clrscr();
+scanf("%d",&yr);
+if("%r %4==0)
+{
+if(yr%100==0)
+{
+if("yr%400==0)
+{
+printf("leapyr");
+}
+else
+{
+printf("not a leap yr");
+}
+else
+{
+printf("not");
+}
+}
+}
+getch();
+}
