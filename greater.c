d69a140b3fd737e4a4f2373203bd869ab94fb79e
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-02-28 16:10:58 +0530

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+int a,b,c;
+clrscr();
+scanf("%d%d%d"",&a,&b,&c);
+if((a>b&&a>=c)
+printf("a is greater");
+elseif(b>=a&&b>=c)
+printf("b is greater");
+else
+printf("c is greater");
+getch();
+}
