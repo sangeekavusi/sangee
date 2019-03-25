d16f5ab8660f011618f24d5f21c6b143e5bc58f2
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-03-25 09:57:42 +0530

    Rename factorial.c to factorialn.c

---
+++ factorialn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+{
+int n,i;
+printf("enter an integer");
+scanf("%d"),&n);
+if(n<0)
+printf("negative number doesn't exist");
+else
+{
+for(i=1;i<=n;i++)
+factorial*=i
+}
+printf("factorial of%d==11% n,factorial");
+return 0;
+}
+}
+}
