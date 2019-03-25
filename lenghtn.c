1e77edb4313a3d240d59ecbf4076264ac758d377
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-03-25 11:48:48 +0530

    Create lenghtn.c

---
+++ lenghtn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h> 
+int main(void) 
+{
+	char s[50],c;
+	int i,j,count=1,max=1,l;
+    gets(s);
+    l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+		for(j=i+1;j<l;j++)
+		if(s[i]==s[j])
+		{
+			count++;
+			if(count>max)
+			{
+				max=count;
+				c=s[i];
+			}
+		}
+ 
+	}
+	printf("\n%c",c);
+	getch();
+}
+ 
