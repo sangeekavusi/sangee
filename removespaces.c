d61ce4f9ba2660c75a0d3f0eda0b7fd0116e9c32
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-03-26 16:01:17 +0530

    Create removespaces.c

---
+++ removespaces.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	int i,j=0;
+	char a[100],b[200];
+	printf("enter the string");
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]!=' ')
+		{
+            b[j]=a[i];
+            j++;
+		}
+	}
+printf("%s",b);
+getch();
+}
