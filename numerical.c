f8b798675c79d934d23c5aaaafab0cf821e50f2e
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-03-26 15:28:13 +0530

    Rename numeric.c to numerical.c

---
+++ numerical.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+float square(float x);
+int main()
+{
+float m,n;
+printf("\n enter the some number for finding sequence\n");
+scanf("%f",&m);
+n=square(m);
+printf("\n square of he given no%f  is %f",m,n);
+{
+float p;
+p=x*x;
+return(p);
+}
