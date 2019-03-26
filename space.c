4022a49e3133f0cf4a87d56bc3046adf8c2ee699
sangeekavusi <47882466+sangeekavusi@users.noreply.github.com>
2019-03-26 15:45:23 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,space;
+clrscr();
+for(i=0;i<=1;i++);
+{
+printf("\nenter the word");
+scanf("%d",&space);
+}
+printf("the word is.....\n");
+scanf("%d",n);
+}
